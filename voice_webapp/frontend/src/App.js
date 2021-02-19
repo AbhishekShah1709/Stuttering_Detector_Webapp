339eb7cbe52e1d87de22c1a6859652d309b875dc
=ssasshah1 <=ssasshah1@gmail.com>
2021-02-19 12:50:26 +0530

    Record and Playing Audio done

--- voice_webapp/frontend/src/App.js
+++ voice_webapp/frontend/src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Recorder from './components/record_play.component.js'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+        <Router>
+            <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to="/" className="navbar-brand">App</Link>
+            <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+            <Link to="/record" className="nav-link">Record</Link>
+            </li>
+            </ul>
+            </div>
+            </nav>
+
+    <Route path="/record" exact component={Recorder}/>
+
     </div>
+    </Router>
   );
 }
 
